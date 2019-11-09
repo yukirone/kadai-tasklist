ed5d5b6d9d3e599aa45ab4f589ef3b45fa2a5a32
yukihiro negishi <moyashi0311@gmail.com>
2019-11-09 07:07:58 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
 Route::get('/','TasksController@index');
-
 Route::resource('tasks','TasksController');
+
+//ユーザ登録のルータ
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
