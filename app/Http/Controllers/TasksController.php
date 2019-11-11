435345ca66c3757e5b699283aca20e6e4cd6d28c
yukihiro negishi <moyashi0311@gmail.com>
2019-11-11 08:21:16 +0000

    retry6

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -68,11 +68,17 @@ class TasksController extends Controller
     public function edit($id)
     {
         $task = Task::find($id);
+        
+        if(\Auth::id() === $task->user_id){
 
         return view('tasks.edit',[
             'task' => $task,
              ]);
     }
+    
+    return redirect('/');
+    
+}
 
     public function update(Request $request, $id)
     {
